63f2f0a0da26e048d9ed9df085032802869e12a9
Kais Hasan <47078826+KaisHasan@users.noreply.github.com>
2019-07-07 22:14:45 +0300

    Add files via upload

---
+++ Country_roads.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef pair<int , int> pi;
+typedef pair<double , int > dpi;
+typedef long long ll;
+typedef unsigned long long ull;
+#define sc(a) scanf("%d",&a)
+#define scd(a) scanf("%lf" , &a)
+#define pb push_back
+const int MAX = 1e5+55;
+const int inf = 1e9+77;
+const int MOD = 1e9+7;
+const double PI = 3.14159265359;
+const double eps = 0.0000001;
+
+struct edge{
+    int w , u , v;
+};
+bool cmp(edge a , edge b){
+    return a.w < b.w;
+}
+vector<pi> MST[MAX];
+vector<edge> edges;
+int rnk[MAX];
+int par[MAX];
+
+void initi(int n){
+    for(int i = 0 ; i < n ; ++i)
+        par[i] = i;
+}
+
+int parent(int v){
+    if(par[v] == v)
+        return v;
+    return par[v] = parent(par[v]);
+}
+
+bool _merge(int u , int v){
+    if(parent(u) == parent(v))
+        return 0;
+    if(rnk[parent(u)] < rnk[parent(v)]){
+        par[parent(u)] = parent(v);
+        rnk[parent(v)] += rnk[parent(u)];
+        return 1;
+    }
+    else
+    {
+        par[parent(v)] = parent(u);
+        rnk[parent(u)] += rnk[parent(v)];
+        return 1;
+    }
+}
+int sum;
+int vis[MAX];
+int dis[MAX];
+void dfs(int v , int mx){
+    vis[v] = 1;
+    for(auto i : MST[v]){
+        if(!vis[i.first]){
+            dis[i.first] = max(mx , i.second);
+            dfs(i.first , max(mx , i.second));
+        }
+    }
+}
+
+int main(){
+int t;
+sc(t);
+for(int o = 1; o <= t ; ++o){
+    memset(rnk , 1 , sizeof(rnk));
+    memset(vis , 0 , sizeof(vis));
+    memset(dis , -1 , sizeof(dis));
+    int n , m;
+    sc(n);
+    sc(m);
+    initi(n);
+    for(int i = 0 ; i < m ;++i){
+        int w , u , v;
+        sc(u);
+        sc(v);
+        sc(w);
+        edges.pb({w , u , v});
+    }
+    sort(edges.begin() , edges.end() , cmp);
+    for(auto i : edges){
+        if(_merge(i.u , i.v)){
+            MST[i.u].pb({i.v , i.w});
+            MST[i.v].pb({i.u , i.w});
+        }
+    }
+    int st;
+    sc(st);
+    dfs(st , -1);
+    dis[st] = 0;
+    printf("Case %d:\n" , o);
+    for(int i = 0 ; i < n ; ++i){
+        if(dis[i] != -1)
+            printf("%d\n" , dis[i]);
+        else
+            printf("Impossible\n");
+    }
+    for(int i = 0 ; i < n ; ++i)
+        MST[i].clear();
+    edges.clear();
+}
+
+
+return 0;
+}
